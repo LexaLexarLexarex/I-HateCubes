fe6f0929e2ad463f4af4136a4a3504f7cab2483e
Dolgopolov Alexey <62442582+LexaLexarLexarex@users.noreply.github.com>
2021-06-01 20:07:06 +0300

    Alll

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -1,13 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GoalScript : MonoBehaviour
 {
+    public AudioSource sphere;
+    public AudioSource cube;
+    public int _Score = 0;
+    [SerializeField] Text scoreText;
+
     private void OnTriggerEnter(Collider other)
     {
         GameObject collideWith = other.gameObject;
         if ( collideWith.tag == gameObject.tag)
         {
             Destroy(other);
+            _Score++;
+            sphere.Play();
+        } else
+        {
+            _Score--;
+            cube.Play();
         }
+    }
+    private void Start()
+    {
+        sphere = GetComponent<AudioSource>();
+        cube = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        scoreText.text = _Score.ToString();
     }
 }
